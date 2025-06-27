a9304c8312910aa83a6a60a0377c95dbc873682e
JethJoe <158308724+JethJoe@users.noreply.github.com>
2025-06-27 08:24:36 +0200

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1751005476016

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_4d7e6c8c._.js",
+  "static/chunks/node_modules_475bb060._.js"
+],
+    source: "dynamic"
+});
