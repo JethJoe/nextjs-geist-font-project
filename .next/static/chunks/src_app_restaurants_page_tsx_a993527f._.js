595a4518eb43054e48d8c037b644548aad6400a4
JethJoe <158308724+JethJoe@users.noreply.github.com>
2025-06-27 03:37:46 +0200

    Add .next/static/chunks/src_app_restaurants_page_tsx_a993527f._.js 1750988265885

---
+++ .next/static/chunks/src_app_restaurants_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_321821c8._.js",
+  "static/chunks/node_modules_dfa9d8c3._.js"
+],
+    source: "dynamic"
+});
